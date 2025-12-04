98f0e08a4313932c360ad48e0bf981b8f012a8b3
rocky <rocky.liu@eternityx.com>
2025-12-04 13:53:04 +0800

    增加sst_file_reader_test测试用例

--- sst_file_reader_test.go
+++ sst_file_reader_test.go
@@ -3,7 +3,7 @@ package grocksdb
 import "testing"
 
 func TestSSTFileReader(t *testing.T) {
-	path := "/tmp/test.sst"
+	path := "/tmp/kvrocks/db/000014.sst"
 	opts := NewDefaultOptions()
 
 	reader := NewSSTFileReader(opts)
@@ -17,8 +17,7 @@ func TestSSTFileReader(t *testing.T) {
 	defer iter.Close()
 
 	for iter.SeekToFirst(); iter.Valid(); iter.Next() {
-		t.Logf("key: %s", string(iter.Key().Data()))
-		t.Logf("value: %s", string(iter.Value().Data()))
+		t.Logf("key: %s value: %s", string(iter.Key().Data()), string(iter.Value().Data()))
 	}
 
 	t.Log("TestSSTFileReader pass")
